fc5f07f74205a24e28cbc9802afcece14d81752e
Andrew Au <iamandrewau@gmail.com>
2018-05-12 19:56:18 -0400

    removed unneeded clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
 var gulp = require('gulp'),
     cleanCSS = require('gulp-clean-css'),
     concat = require('gulp-concat'),
-    injectfile = require('gulp-inject-file'),
-    clean = require('gulp-clean');
- 
+    injectfile = require('gulp-inject-file');
+
 gulp.task('css-concat-min', function() {
   return gulp.src('./stylesheets/*.css')
     .pipe(concat('merged-min.css'))
@@ -19,11 +18,6 @@ gulp.task('css-inject', function() {
             .pipe(gulp.dest('./'));
 });
 
-gulp.task('clean', function() {
-    return gulp.src('./index.html', {read: false})
-        .pipe(clean());
-});
-
 gulp.task('default', function() {
-    gulp.start('css-concat-min', 'clean', 'css-inject');
+    gulp.start('css-concat-min', 'css-inject');
 });
