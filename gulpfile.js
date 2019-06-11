2fb200e4586fd4914cec4d7597a7db912089648b
Andrew Au <iamandrewau@gmail.com>
2019-06-11 16:34:04 -0400

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,21 @@ var gulp = require('gulp'),
     injectfile = require('gulp-inject-file');
 
 gulp.task('css-concat-min', function() {
-  return gulp.src('./stylesheets/*.css')
-    .pipe(concat('merged-min.css'))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./dist/'));
+    gulp.src('./stylesheets/*.css')
+        .pipe(concat('merged-min.css'))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('css-inject', function() {
-    return gulp.src('./amp/*.html')
-            .pipe(injectfile({
-                pattern: '<!--\\s*inject:\\s*<filename>-->'
-            }))
-            .pipe(gulp.dest('./'));
+    gulp.src('./amp/*.html')
+        .pipe(injectfile({
+            pattern: '<!--\\s*inject:\\s*<filename>-->'
+        }))
+        .pipe(gulp.dest('./'));
 });
 
-gulp.task('default', function() {
-    gulp.start('css-concat-min', 'css-inject');
+gulp.task('default', function(done) {
+    gulp.task('start', gulp.series('css-concat-min', 'css-inject'));
+    done();
 });
